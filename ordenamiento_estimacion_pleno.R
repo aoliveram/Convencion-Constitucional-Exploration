d6026b3794728850c028658409dccca2aff8d2ea
aoliveram <anibal.olivera.m@gmail.com>
2025-02-19 14:34:42 -0300

    estimación ordenamiento político usando W-Nominate

---
+++ ordenamiento_estimacion_pleno.R
@@ -0,0 +1,75 @@
+#------------------------------------------------------------------------------
+# Estimación de Ordenamiento Político usando W-Nominate 
+#------------------------------------------------------------------------------
+
+install.packages('wnominate')
+library(wnominate)
+
+# Votaciones en el pleno hasta el 14 de Agosto de 2021. 147 Votaciones
+votaciones_al_14ago2021 <- read.csv("rcp_convencion/RCP_votaciones_al_14ago2021.csv")
+votaciones_al_14ago2021 <- votaciones_al_14ago2021[,-1] # Quito la primera columna
+
+# Guardo los votantes y quito columna
+votantes <- votaciones_al_14ago2021[,1]
+votaciones_al_14ago2021 <- votaciones_al_14ago2021[,-1]
+
+# Crear un objeto de clase rollcall para el análisis con wnominate
+votaciones_al_14ago2021_rc <- rollcall(
+  votaciones_al_14ago2021,             
+  yea = c(1),
+  nay = c(0),
+  missing = c(NA),
+  notInLegis = NULL,
+  legis.names = votantes,
+  desc = "Votaciones Convención Constitucional al 14 Ago 2021"
+)
+
+# Ejecutar el análisis wnominate
+ordenamiento_al_14ago2021 <- wnominate(
+  votaciones_al_14ago2021_rc, 
+  dims = 2, 
+  polarity = c(18,18)
+)
+
+# Gráfico de los resultados
+plot(ordenamiento_al_14ago2021)
+
+# Creamos data frame con los resultados de la dimensión 1.
+ordenamiento_al_14ago2021_1D <- data.frame(posicion_ideologica = ordenamiento_al_14ago2021$legislators$coord1D)
+
+# Reescalamos dentro de [-1,1]
+reescalar <- function(vector_original) {
+  min_original <- min(vector_original)
+  max_original <- max(vector_original)
+  a <- -1
+  b <- 1
+  vector_reescalado <- ((vector_original - min_original) / (max_original - min_original)) * (b - a) + a
+  return(vector_reescalado)
+}
+
+library(dplyr)
+
+ordenamiento_al_14ago2021_1D <- reescalar(ordenamiento_al_14ago2021_1D) %>%
+  mutate(nombre_votante = votantes, n_votante = as.character(1:155)) %>%
+  arrange(posicion_ideologica)
+
+print(ordenamiento_al_14ago2021_1D)
+
+# Grabamos los índices de cada candidato (equivalente a las posiciones de izquierda a derecha)
+ordenamiento_al_14ago2021_1D$posicion_izq_der <- row.names(ordenamiento_al_14ago2021_1D)
+
+# Graficamos de mayor a menor las posiciones de los votantes
+library(ggplot2)
+ggplot(ordenamiento_al_14ago2021_1D, aes(x = as.numeric(posicion_izq_der), y = posicion_ideologica, color = posicion_ideologica)) +
+  geom_point() +
+  geom_text(aes(label = n_votante), vjust = -1) +
+  theme(axis.title.x = element_blank(), axis.text.x = element_blank())+
+  ggtitle("W-NOMINATE")+
+  theme(plot.title = element_text(hjust = 0.5))+
+  labs(y = "Posición ideológica")+
+  theme(axis.title.y = element_text(size = 15))+
+  scale_color_gradient(low = "red", high = "blue")
+
+
+
+
